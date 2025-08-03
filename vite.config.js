90de7851a083efe440889b4bb2c1a782a5fd4b7d
Atharva Mhatre <89691657+Atharvamhatre149@users.noreply.github.com>
2025-08-03 16:16:08 +0530

    npm fixes for tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,17 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { defineConfig } from 'vite'
+import path from "path"
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      plugins: [
-        commonjs(),
-        nodeResolve({
-          preferBuiltins: true
-        })
-      ],
-      output: {
-        manualChunks: {
-          'vendor': [
-            'react',
-            'react-dom',
-            'react-router-dom',
-            'framer-motion',
-            'axios',
-            'zustand'
-          ],
-        },
-      },
-    },
-    target: 'es2015',
-    sourcemap: false,
-    minify: 'terser',
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom', 'framer-motion', 'axios', 'zustand'],
-  },
-});
+})
